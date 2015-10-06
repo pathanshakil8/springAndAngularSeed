120b0dd646a89383f87ecf7e257562164c77e447
MohitVirmani <mohit.virmani@oodlestechnologies.com>
2015-10-06 19:47:15 +0530

    first commit

---
+++ src/main/java/com/springAndAngularSeed/SpringAndAngularSeedApplication.java
@@ -0,0 +1,12 @@
+package com.springAndAngularSeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndAngularSeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndAngularSeedApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springAndAngularSeed/SpringAndAngularSeedApplicationTests.java
@@ -0,0 +1,18 @@
+package com.springAndAngularSeed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringAndAngularSeedApplication.class)
+@WebAppConfiguration
+public class SpringAndAngularSeedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
